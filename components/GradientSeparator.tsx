58fbd1d3f234650876497d2a15b9bdc37bd5209e
fayinana <fekedeananiya@gmail.com>
2025-09-01 15:13:00 +0300

    remove comment

--- components/GradientSeparator.tsx
+++ components/GradientSeparator.tsx
@@ -1,31 +1,9 @@
-// import React from "react";
-
-// interface GradientSeparatorProps {
-//   width?: string; // Tailwind width, e.g., "w-full"
-//   height?: string; // Tailwind height, e.g., "h-[1px]"
-//   color?: string; // Tailwind gradient color, e.g., "via-secondary-foreground/10"
-//   className?: string;
-// }
-
-// export const GradientSeparator: React.FC<GradientSeparatorProps> = ({
-//   width = "w-full",
-//   height = "h-[1px]",
-//   color = "via-secondary-foreground/10",
-//   className = "",
-// }) => {
-//   return (
-//     <div
-//       className={`${width} ${height} bg-gradient-to-r from-transparent ${color} to-transparent ${className}`}
-//     />
-//   );
-// };
-
 import React from "react";
 
 interface GradientSeparatorProps {
-  width?: string; // Tailwind width, e.g., "w-full"
-  height?: string; // Tailwind height, e.g., "h-[1px]"
-  color?: string; // Tailwind gradient color, e.g., "from-transparent via-gray-400 to-transparent"
+  width?: string;
+  height?: string;
+  color?: string;
   className?: string;
   variant?: "gradient" | "dotted";
 }
