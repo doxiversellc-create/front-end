5ff9fc9bd0a050f1ba5649692b97a65915461e8c
fayinana <fekedeananiya@gmail.com>
2025-09-03 00:53:07 +0300

    add suspence on FDATable

--- app/fda-update/page.tsx
+++ app/fda-update/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import FDATable from "./_components/FDATable";
 import Hero from "./_components/Header";
 
@@ -6,7 +7,9 @@ const Index = () => {
     <div className="min-h-screen px-6 md:px-12 lg:px-20">
       <div className="absolute top-0 left-0 w-full h-[50vh] bg-gradient-to-b from-primary/25 to-transparent pointer-events-none -z-10" />
       <Hero />
-      <FDATable />
+      <Suspense fallback={<div>Loading...</div>}>
+        <FDATable />
+      </Suspense>
     </div>
   );
 };
