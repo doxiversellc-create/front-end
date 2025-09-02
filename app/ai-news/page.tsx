5174d5ee94047cfbf0616ab4be40878e1018cbb6
fayinana <fekedeananiya@gmail.com>
2025-09-02 23:14:03 +0300

    fix the subscription sextion border ind background

--- app/ai-news/page.tsx
+++ app/ai-news/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { Suspense } from "react";
 import Hero from "./_components/Hero";
 import NewsGrid from "./_components/NewsGrid";
@@ -15,16 +16,39 @@ export default function NewsroomPage() {
           </Suspense>
         </section>
 
-        <div className=" w-full py-12 md:py-16 space-y-4 items-center flex justify-center flex-col  bg-gradient-to-b to-primary/25 from-transparent -z-10 rounded-2xl border-2 border-white shadow px-2 md:px-4 border-t-0">
-          <h2 className="text-2xl text-center font-medium max-w-[603px] md:text-3xl font-outfit lg:text-4xl leading-tight  mx-auto">
-            <span className="text-center">Subscribe to Get Notified About Our Latest AI News</span>
-          </h2>
-          <p className="text-pretty  text-center font-inter md:text-base lg:text-lg  leading-relaxed max-w-[749px] mx-auto">
-            Stay updated with AI breakthroughs, trends, and expert tips delivered straight to your
-            inbox.
-          </p>
-          <SubscribeInput />
-        </div>
+        <section className=" w-full p-4 md:px-6 lg:px-8 lg:pb-10 ">
+          <div className="min-w-full relative px-2 pb-2 rounded-3xl ">
+            <div className="bg-gradient-to-b from-black/0  to-border p-[1px] rounded-3xl">
+              <div className="bg-background pb-1.5 px-1.5 rounded-3xl">
+                <div className="relative min-w-full rounded-3xl px-8 md:px-12 lg:px-16 pb-8 md:pb-12 lg:pb-16 overflow-hidden bg-background">
+                  <div className="absolute bottom-0 left-1/2 z-10 -translate-x-1/2">
+                    <Image
+                      src="/shapes/top-category.svg"
+                      className="invert brightness-0 rotate-180"
+                      alt="top-category"
+                      width={200}
+                      height={100}
+                    />
+                  </div>
+                  <div className="bg-gradient-to-b absolute inset-0  from-background/30 via-[#A9D5F0]/20 to-[#A9D5F0]" />
+
+                  <div className="relative z-10 text-center space-y-5 mt-5 pb-7 md:pb-3">
+                    <h2 className="text-3xl mt-6 lg:mt-12 max-w-[546px] md:text-4xl font-semibold font-outfit lg:text-5xl leading-tight  mx-auto">
+                      Subscribe to Get Notified About Our Latest AI News
+                    </h2>
+                    <p className="text-lg max-w-[569px] mx-auto lg:text-xl opacity-80 font-inter">
+                      Stay updated with AI breakthroughs, trends, and expert tips delivered straight
+                      to your inbox.
+                    </p>
+                    <div className="flex mx-auto mt-8 justify-center">
+                      <SubscribeInput />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
     </div>
   );
