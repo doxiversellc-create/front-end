ecbedfd09aa7248ad87293620ea7f5c1b06b0494
Sallah26 <143meski@gmail.com>
2025-09-02 18:15:57 +0300

    Update font in SocialMediaSection component for improved styling

--- app/newsletter-subscription/_components/SocialMediaSection.tsx
+++ app/newsletter-subscription/_components/SocialMediaSection.tsx
@@ -1,10 +1,9 @@
 import { socialMediaData, SocialMediaIcon } from "@/components/Footer";
-import React from "react";
 
 export default function SocialMediaSection() {
   return (
     <div className="flex flex-col justify-center items-center gap-2 my-8 md:my-12">
-      <p className="text-base mt-3 font-inter">Follow for more!</p>
+      <p className="text-base mt-3 font-outfit">Follow for more!</p>
       <div className="flex gap-4">
         {socialMediaData.map(item => (
           <SocialMediaIcon key={item.name} icon={item.icon} name={item.name} href={item.href} />
