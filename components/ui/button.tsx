828dde4bec8046bafe0203ab60a7c61d7e81c2c4
Sallah26 <143meski@gmail.com>
2025-09-03 10:55:19 +0300

    Resolved Copilot comments

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,7 +5,7 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex font-inter items-center justify-center gap-2 whitespace-nowrap rounded-full font-inter text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 aria-invalid:border-destructive",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-full font-inter text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 aria-invalid:border-destructive",
   {
     variants: {
       variant: {
@@ -18,7 +18,7 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-11 lg:h-12 px-6 py-3 ",
+        default: "h-11 lg:h-12 px-6 py-3",
         sm: "h-9 md:h-10 gap-1.5 px-5 has-[>svg]:px-2.5",
         lg: "h-11 md:h-14 px-6 has-[>svg]:px-4",
         icon: "size-9 md:size-11",
