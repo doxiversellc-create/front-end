220e21dcf1fe9d66e25bd54d77a4665fbcb80160
fayinana <fekedeananiya@gmail.com>
2025-09-03 18:56:23 +0300

    apply filter and fix some spacings

--- app/ai-jobs/_components/JobCard.tsx
+++ app/ai-jobs/_components/JobCard.tsx
@@ -1,9 +1,10 @@
 "use client";
-
-import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
-import { Bookmark, CirclePoundSterling, ExternalLink, MapPin } from "lucide-react";
 import { useState } from "react";
+
+import { Bookmark, CirclePoundSterling, ExternalLink, MapPin } from "lucide-react";
+
+import { Card } from "@/components/ui/card";
+import Link from "next/link";
 
 interface JobCardProps {
   title: string;
@@ -34,20 +35,20 @@ const JobCard = ({
   const displayedText = expanded ? description : description.slice(0, maxChars);
 
   return (
-    <Card className="p-8 border-0 rounded-none">
-      <div className="flex justify-between items-start mb-4">
+    <Card className="p-4 md:p-8 border-0 rounded-none not-last:border-b">
+      <div className="flex justify-between items-start">
         <div className="flex-1">
-          <div className="flex items-center text-sm text-muted-foreground mb-2">
+          <div className="flex items-center text-sm text-muted-foreground ">
             <span>
-              By <span className="font-medium text-foreground">{author}</span>
+              By <span className="font-semibold text-foreground">{author}</span>
             </span>
             <span className="mx-2">•</span>
-            <span>Published on {publishedDate}</span>
+            <span className="text-sm">Published on {publishedDate}</span>
           </div>
-          <h2 className="text-xl font-outfit font-medium mb-2 cursor-pointer transition-colors">
+          <h2 className="md:text-xl text-lg font-outfit font-medium mb-1 cursor-pointer transition-colors">
             {title}
           </h2>
-          <p className="text-muted-foreground text-sm leading-relaxed mb-2">
+          <p className="text-muted-foreground text-sm md:text-base leading-relaxed">
             {displayedText}
             {!expanded && shouldTruncate && "..."}
           </p>
@@ -60,21 +61,19 @@ const JobCard = ({
             </button>
           )}
         </div>
-        <Button variant="ghost" size="icon" className="ml-4 shrink-0">
-          <Bookmark className="h-4 w-4" />
-        </Button>
+        <Bookmark className="h-5 w-5" />
       </div>
 
-      <div className="flex flex-wrap gap-4 mb-4">
+      <div className="flex flex-wrap gap-4">
         {tags.map(tag => (
-          <span className="text-muted-foreground" key={tag}>
+          <span className="text-muted-foreground text-sm" key={tag}>
             #{tag}
           </span>
         ))}
       </div>
 
-      <div className="flex items-center justify-between">
-        <div className="flex items-center space-x-4 text-sm text-muted-foreground">
+      <div className="flex flex-col md:flex-row md:items-center md:justify-between flex-wrap">
+        <div className="flex items-center flex-wrap gap-x-2 gap-y-1 text-sm text-muted-foreground">
           {salary && (
             <div className="flex items-center space-x-1">
               <CirclePoundSterling className="h-5 w-5 text-foreground" />
@@ -83,17 +82,21 @@ const JobCard = ({
           )}
           <div className="flex items-center space-x-1">
             <span className="text-muted-foreground/50">•</span>
-            <span className="pl-2">{type}</span>
+            <span className="mr-1 pl-1">{type}</span>
           </div>
-          <div className="flex items-center space-x-1 border-l-2 pl-2">
+          <div className="flex items-center space-x-1 border-l-2 pl-1">
             <MapPin className="h-4 w-4 text-foreground" />
             <span>{location}</span>
           </div>
         </div>
-        <span className="text-brand border-brand hover:bg-brand underline flex items-center gap-1 cursor-pointer">
+
+        <Link
+          href="/"
+          className="mt-3 md:mt-0 text-brand border-brand hover:bg-brand underline flex items-center gap-1 cursor-pointer self-start md:self-auto"
+        >
           <span>Visit Site</span>
           <ExternalLink className="w-4 h-4" />
-        </span>
+        </Link>
       </div>
     </Card>
   );
--- app/ai-jobs/_components/Sidebar.tsx
+++ app/ai-jobs/_components/Sidebar.tsx
@@ -1,38 +1,38 @@
 import { Card } from "@/components/ui/card";
 
 const Sidebar = () => {
-  const topTags = ["#Dentist", "#Anesthesia", "#Nursing", "#Psychiatrist", "#Radiology"];
+  const topTags = ["Dentist", "Anesthesia", "Nursing", "Psychiatrist", "Radiology"];
 
   return (
     <aside className="space-y-2">
-      <Card className="p-2 shadow-none border-none">
-        <h3 className="font-semibold font-outfit">Top Tagged Jobs</h3>
+      <Card className="shadow-none border-none">
+        <h3 className="font-semibold font-outfit text-lg">Top Tagged Jobs</h3>
         <div className="flex flex-wrap gap-2">
           {topTags.map(tag => (
             <span
               key={tag}
-              className="px-3 py-1 bg-secondary text-secondary-foreground/80 rounded-full text-xs md:text-sm font-medium transition-colors hover:bg-secondary/80"
+              className="px-3 py-1 text-secondary-foreground/80 rounded-full text-sm font-medium transition-colors hover:bg-secondary/80"
             >
-              {tag}
+              #{tag}
             </span>
           ))}
         </div>
       </Card>
 
-      <Card className="p-4 shadow-none border-none">
-        <h3 className="font-semibold font-outfit">About our Job Sources</h3>
-        <p className="text-sm text-muted-foreground leading-relaxed">
+      <Card className="shadow-none border-none">
+        <h3 className="font-semibold text-lg font-outfit">About our Job Sources</h3>
+        <p className="text-sm md:text-base text-muted-foreground leading-relaxed">
           At Doxiverse, we aggregate healthcare-related AI and healthcare job listings from trusted
           industry sources, carefully selected for their reliability and expertise. Our goal is to
           connect you with opportunities that reflect the latest advancements in patient care and
           medical technology, ensuring each role has impactful and credible.
         </p>
-        <p className="text-sm text-muted-foreground leading-relaxed">
+        <p className="text-sm md:text-base text-muted-foreground leading-relaxed">
           We update listings daily to deliver fresh opportunities, connecting talent with
           cutting-edge healthcare solutions and empowering you to thrive in this evolving field.
         </p>
         <div className="">
-          <div className="flex space-x-4 text-xs text-muted-foreground">
+          <div className="flex space-x-4 text-sm text-muted-foreground">
             <a href="/terms" className="hover:text-foreground transition-colors">
               Terms of Use
             </a>
