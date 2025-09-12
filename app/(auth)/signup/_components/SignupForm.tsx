7a40a454836a4da4391ee610e9a8441aa02d1c8c
Abubeker Abduljelil <Abubekera87@gmail.com>
2025-09-12 18:02:33 +0300

    refactor: refactor signup form

--- app/(auth)/signup/_components/SignupForm.tsx
+++ app/(auth)/signup/_components/SignupForm.tsx
@@ -7,21 +7,14 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import { ArrowRight, Eye, EyeOff, Loader2 } from "lucide-react";
+import { ArrowRight, Loader2 } from "lucide-react";
 import { toast } from "sonner";
 import * as z from "zod";
 
 import GoogleAuthButton from "@/app/(auth)/_components/GoogleAuthButton";
+import SignupFormFields from "@/app/(auth)/signup/_components/SignupFormFields";
 import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+import { Form } from "@/components/ui/form";
 import { useSignup } from "@/hooks/authHooks/useSignup";
 import { signupFormSchema } from "@/lib/schemas/auth.schema";
 
@@ -87,136 +80,12 @@ const SignupForm = () => {
         <div className="mb-14 flex flex-col gap-8">
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="flex w-full flex-col gap-5">
-              <div className="flex gap-3">
-                {/* First Name */}
-                <FormField
-                  control={form.control}
-                  name="firstName"
-                  render={({ field }) => (
-                    <FormItem className="flex-1">
-                      <FormLabel>
-                        First Name <span className="text-primary">*</span>
-                      </FormLabel>
-                      <FormControl>
-                        <Input placeholder="Eg, Peter" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                {/* Last Name */}
-                <FormField
-                  control={form.control}
-                  name="lastName"
-                  render={({ field }) => (
-                    <FormItem className="flex-1">
-                      <FormLabel>
-                        Last Name <span className="text-primary">*</span>
-                      </FormLabel>
-                      <FormControl>
-                        <Input placeholder="Eg, Lost" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-
-              {/* Username */}
-              <FormField
-                control={form.control}
-                name="username"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>
-                      Username <span className="text-primary">*</span>
-                    </FormLabel>
-                    <FormControl>
-                      <Input type="text" placeholder="john_doe" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              {/* Email */}
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>
-                      Email <span className="text-primary">*</span>
-                    </FormLabel>
-                    <FormControl>
-                      <Input type="email" placeholder="Example@email.com" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              {/* Password */}
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>
-                      Password <span className="text-primary">*</span>
-                    </FormLabel>
-                    <FormControl>
-                      <div className="relative flex items-center">
-                        <Input
-                          type={passwordVisible ? "text" : "password"}
-                          placeholder="********"
-                          {...field}
-                        />
-                        <Button
-                          variant={"ghost"}
-                          size={"icon"}
-                          type="button"
-                          className="absolute right-2 hover:bg-transparent"
-                          onClick={() => setPasswordVisible(!passwordVisible)}
-                        >
-                          {passwordVisible ? <Eye /> : <EyeOff />}
-                        </Button>
-                      </div>
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              {/* Confirm Password */}
-              <FormField
-                control={form.control}
-                name="confirmPassword"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>
-                      Confirm Password <span className="text-primary">*</span>
-                    </FormLabel>
-                    <FormControl>
-                      <div className="relative flex items-center">
-                        <Input
-                          type={confirmPasswordVisible ? "text" : "password"}
-                          placeholder="********"
-                          {...field}
-                        />
-                        <Button
-                          variant={"ghost"}
-                          size={"icon"}
-                          type="button"
-                          className="absolute right-2 hover:bg-transparent"
-                          onClick={() => setConfirmPasswordVisible(!confirmPasswordVisible)}
-                        >
-                          {confirmPasswordVisible ? <Eye /> : <EyeOff />}
-                        </Button>
-                      </div>
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
+              <SignupFormFields
+                confirmPasswordVisible={confirmPasswordVisible}
+                form={form}
+                passwordVisible={passwordVisible}
+                setPasswordVisible={setPasswordVisible}
+                setConfirmPasswordVisible={setConfirmPasswordVisible}
               />
               <Button type="submit" className="mt-4 rounded-md" disabled={isLoading}>
                 {isLoading ? <Loader2 className="mr-2 animate-spin" /> : "Sign Up"}
