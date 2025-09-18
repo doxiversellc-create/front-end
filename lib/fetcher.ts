c427eb51eabcfbcddb57d64d71cb9c17937ea3a5
Faysel Abdella <fayselcode@gmail.com>
2025-09-18 09:16:52 +0300

    re-add NEXT_PUBLIC_API_BASE_URL

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -22,10 +22,7 @@ export interface FetchResponse<T> {
 }
 
 const API_BASE_URL = (() => {
-  const baseUrl =
-    process.env.API_BASE_URL ||
-    process.env.API_BASE_URL ||
-    "https://doxiverse-server-production.up.railway.app/api";
+  const baseUrl = process.env.API_BASE_URL || process.env.NEXT_PUBLIC_API_BASE_URL;
   if (baseUrl) return baseUrl;
   if (process.env.NODE_ENV === "development") {
     return "http://localhost:3000/api";
