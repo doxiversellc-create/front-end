1ec1919fd646e5e0f1c1f1e2f94827171d855c3c
Abubeker Abduljelil <Abubekera87@gmail.com>
2025-09-19 18:24:14 +0300

    feat: replace auth pages image with the new one

--- app/(auth)/_components/TitleImage.tsx
+++ app/(auth)/_components/TitleImage.tsx
@@ -5,10 +5,8 @@ import Logo from "@/components/Logo";
 const TitleImage = () => {
   return (
     <div className="relative flex h-full items-center overflow-hidden rounded-l-2xl rounded-r-3xl">
-      <Image src={"/auth_page_image.webp"} alt="auth page image" fill className="object-fill" />
-      <div className="absolute top-10 flex w-full items-center justify-center">
-        <Logo />
-      </div>
+      <Image src={"/auth_page_image.webp"} alt="auth page image" fill className="object-cover" />
+
       <div className="absolute top-0 flex h-full w-full items-center justify-center">
         <div className="flex max-w-md flex-col gap-5 text-center max-lg:px-2">
           <h1 className="font-outfit text-3xl font-semibold lg:text-4xl xl:text-5xl">
@@ -18,6 +16,9 @@ const TitleImage = () => {
             Founded by physicians, Doxiverse is your one-stop hub for healthcare AI tools.
           </p>
         </div>
+      </div>
+      <div className="absolute top-10 flex w-full items-center justify-center">
+        <Logo />
       </div>
     </div>
   );
