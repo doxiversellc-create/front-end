07c02dcaa29d49a711976bffc952135318a6b1e6
fayinana <fekedeananiya@gmail.com>
2025-10-03 11:55:30 +0300

    add loading skeleton for documents page

--- app/(main)/documents/terms-of-use/page.tsx
+++ app/(main)/documents/terms-of-use/page.tsx
@@ -1,5 +1,4 @@
 import { fetchPageContent } from "@/actions/content.actions";
-import DocumentLayout from "../_components/DocumentLayout";
 export async function generateMetadata() {
   const { content } = await fetchPageContent("legal/terms-of-use");
 
@@ -15,15 +14,20 @@ export default async function TermsOfUsePage() {
   const { content } = await fetchPageContent("legal/terms-of-use");
 
   return (
-    <DocumentLayout title={content.title}>
-      <section className="space-y-5">
-        <div
-          className="prose font-inter max-w-none space-y-3 text-sm leading-relaxed tracking-wide md:text-base"
-          dangerouslySetInnerHTML={{
-            __html: content.content || "",
-          }}
-        />
-      </section>
-    </DocumentLayout>
+    <article className="prose prose-lg max-w-none space-y-6 lg:space-y-10">
+      <h1 className="font-outfit text-2xl font-semibold md:text-4xl">{content.title}</h1>
+
+      <div className="space-y-5">
+        {" "}
+        <section className="space-y-5">
+          <div
+            className="prose font-inter max-w-none space-y-3 text-sm leading-relaxed tracking-wide md:text-base"
+            dangerouslySetInnerHTML={{
+              __html: content.content || "",
+            }}
+          />
+        </section>
+      </div>
+    </article>
   );
 }
