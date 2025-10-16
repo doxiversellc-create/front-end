5184d2dced31a000cb2c359b9c7a420e7a9fb30f
Abubeker Abduljelil <Abubekera87@gmail.com>
2025-10-16 20:51:40 +0300

    feat: update logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,8 +3,7 @@ import Link from "next/link";
 
 const Logo = () => (
   <Link href="/" className="flex items-center gap-2">
-    <Image src="/logo.svg" alt="Doxiverse Logo" width={32} height={32} className="h-8 w-8" />
-    <span className="font-outfit text-2xl font-medium">Doxiverse</span>
+    <Image src="/main-logo.svg" alt="Doxiverse Logo" width={150} height={100} />
   </Link>
 );
 
